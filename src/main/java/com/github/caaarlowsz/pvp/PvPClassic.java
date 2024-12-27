e37cf7f93d156e45869fd155823d2a69c0cfc708
Caaarlowsz <caaarlowsz@outlook.com>
2024-12-27 10:05:16 -0300

    Adding player interface wrapper.

--- src/main/java/com/github/caaarlowsz/pvp/PvPClassic.java
+++ src/main/java/com/github/caaarlowsz/pvp/PvPClassic.java
@@ -1,11 +1,34 @@
 package com.github.caaarlowsz.pvp;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.UUID;
+
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PvPClassic extends JavaPlugin {
+
+	private static LinkedHashMap<UUID, PvPPlayer> playerMap = new LinkedHashMap<>();
+
+	public static Collection<PvPPlayer> getPlayers() {
+		return Collections.unmodifiableCollection(playerMap.values());
+	}
+
+	public static PvPPlayer getPlayer(Player player) {
+		PvPPlayer pvpPlayer = playerMap.get(player.getUniqueId());
+		if (pvpPlayer == null)
+			playerMap.put(player.getUniqueId(), pvpPlayer = new PvPPlayer(player));
+		return pvpPlayer;
+	}
+
+	public static void removePlayer(Player player) {
+		playerMap.remove(player.getUniqueId());
+	}
 
 	public static PvPClassic getPlugin() {
 		return getPlugin(PvPClassic.class);
---
+++ src/main/java/com/github/caaarlowsz/pvp/PvPPlayer.java
@@ -0,0 +1,34 @@
+package com.github.caaarlowsz.pvp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.craftbukkit.v1_8_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class PvPPlayer extends CraftPlayer {
+
+	public PvPPlayer(Player player) {
+		super((CraftServer) Bukkit.getServer(), ((CraftPlayer) player).getHandle());
+	}
+
+	public void reset() {
+		this.setExp(0);
+		this.setLevel(0);
+		this.setFireTicks(0);
+		this.setExhaustion(0);
+		this.setFoodLevel(20);
+		this.setSaturation(0);
+		this.setNoDamageTicks(0);
+		this.setHealthScale(20);
+		this.setMaxHealth(20);
+		this.setHealth(20);
+		this.setGameMode(GameMode.ADVENTURE);
+		this.getActivePotionEffects().forEach(e -> this.removePotionEffect(e.getType()));
+		this.setAllowFlight(false);
+		this.setFlying(false);
+
+		this.getInventory().setArmorContents(null);
+		this.getInventory().clear();
+	}
+}
